b73c2b3b68f9dc766eb6dc3f3966ab92f8f1a152
Trong-7984 <trongnqq7984@ut.edu.vn>
2026-01-16 14:43:06 +0700

    Add files via upload

---
+++ getAdjList.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+#include <utility>
+
+using namespace std;
+
+vector<vector<pair<int,int>>> adj;
+
+vector<vector<pair<int,int>>> getAdjList() {
+    return adj;
+}
---
+++ graph.h
@@ -0,0 +1,35 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Graph {
+private:
+    int n;                                      // số đỉnh
+    bool directed;                             // đồ thị có hướng hay không
+    vector<vector<pair<int,int>>> adj;         // danh sách kề
+
+public:
+    // Khởi tạo đồ thị
+    Graph(int n, bool directed = false) {
+        this->n = n;
+        this->directed = directed;
+        adj.resize(n);
+    }
+
+    // Thêm cạnh (u -> v) với trọng số w
+    void addEdge(int u, int v, int w = 1) {
+        adj[u].push_back({v, w});
+        if (!directed) {
+            adj[v].push_back({u, w});
+        }
+    }
+
+    // Lấy danh sách kề
+    vector<vector<pair<int,int>>> getAdjList() {
+        return adj;
+    }
+};
+
+#endif // GRAPH_H
