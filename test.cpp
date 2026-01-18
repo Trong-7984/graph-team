7c4a945858332538d0751bb5a0a1d8d1a5331bd2
thainv0467 <thainv0467@ut.edu.vn>
2026-01-18 09:56:00 +0700

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "Graph.h" // File định nghĩa lớp Graph của bạn
+
+// Khai báo prototype các hàm nếu chúng nằm ở file .cpp khác
+void BFS(Graph& g, int startNode);
+void DFS(Graph& g, int startNode);
+bool isBipartite(Graph& g);
+
+using namespace std;
+// --- CHƯƠNG TRÌNH TEST NHANH ---
+int main(){
+// Tạo đồ thị có 5 đỉnh (0 đến 4)
+    Graph  g(5);
+
+    // Thêm các cạnh (u, v)
+    g.addEdge(0, 1);
+    g.addEdge(0, 2);
+    g.addEdge(1, 3);
+    g.addEdge(2, 4);
+    // Gọi các hàm để kiểm tra
+    BFS(g, 0);
+    DFS(g, 0);
+
+    if (isBipartite(g)) {
+        cout << "Do thi la do thi phan doi." << endl;
+    }
+    else {
+        cout << "Do thi KHONG phai la do thi phan doi." << endl;
+    }
+
+    return 0;
